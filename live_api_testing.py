371793362c197f428b3165364ebea4751a10f879
Chris Gokus <lightheaded@gmx.de>
2024-10-12 14:45:04 +0200

    removed some files from dvc + live testing

--- live_api_testing.py
+++ live_api_testing.py
@@ -1,6 +1,25 @@
 import requests
 
-response = requests.post('cg-census-pred-app.herokuapp.com')
+test_data = {
+        'age': 50,
+        'workclass': 'Private',
+        'fnlgt': 280464,
+        'education': 'Some-college',
+        'education-num': 10,
+        'marital-status': 'Married',
+        'occupation': 'Exec-managerial',
+        'relationship': 'Husband',
+        'race': 'White',
+        'sex': 'Male',
+        'capital-gain': 0,
+        'capital-loss': 0,
+        'hours-per-week': 60,
+        'native-country': 'United-States'
+    }
+
+response = requests.post('https://cg-census-pred-app-prod-e1af23d4a8.herokuapp.com/', json=test_data)
+print(response.status_code)
+assert response.status_code == 200
 
 print(response.status_code)
 print(response.json())
